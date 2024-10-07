b1719bf717a7e80dd021ea3ca446a7acac4bd68b
Ernesto ID <ernestojavadeveloper@gmail.com>
2024-10-07 12:56:01 +0200

    feat: update tests add new tests

--- src/test/java/com/shop/pricing/PriceIntegrationTests.java
+++ src/test/java/com/shop/pricing/PriceIntegrationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,12 +19,13 @@ public class PriceIntegrationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    private static final String URL = "/shop/search/prices";
     private static final String BRAND_ID = "1";
     private static final String PRODUCT_ID = "35455";
 
     @Test
-    public void test1() throws Exception {
-        mockMvc.perform(get("/shop/search/price")
+    public void testEarlyThan15ReturnPriceList1() throws Exception {
+        mockMvc.perform(get(URL)
                         .param("brandId", BRAND_ID)
                         .param("productId", PRODUCT_ID)
                         .param("dateTime", "2020-06-14T10:00:00"))
@@ -32,8 +34,8 @@ public class PriceIntegrationTests {
     }
 
     @Test
-    public void test2() throws Exception {
-        mockMvc.perform(get("/shop/search/price")
+    public void testReturnHigherPriority() throws Exception {
+        mockMvc.perform(get(URL)
                         .param("brandId", BRAND_ID)
                         .param("productId", PRODUCT_ID)
                         .param("dateTime", "2020-06-14T16:00:00"))
@@ -42,8 +44,8 @@ public class PriceIntegrationTests {
     }
 
     @Test
-    public void test3() throws Exception {
-        mockMvc.perform(get("/shop/search/price")
+    public void testLateThan1830ReturnPriceList1() throws Exception {
+        mockMvc.perform(get(URL)
                         .param("brandId", BRAND_ID)
                         .param("productId", PRODUCT_ID)
                         .param("dateTime", "2020-06-14T21:00:00"))
@@ -52,8 +54,8 @@ public class PriceIntegrationTests {
     }
 
     @Test
-    public void test4() throws Exception {
-        mockMvc.perform(get("/shop/search/price")
+    public void testReturnPriceList3() throws Exception {
+        mockMvc.perform(get(URL)
                         .param("brandId", BRAND_ID)
                         .param("productId", PRODUCT_ID)
                         .param("dateTime", "2020-06-15T10:00:00"))
@@ -62,12 +64,47 @@ public class PriceIntegrationTests {
     }
 
     @Test
-    public void test5() throws Exception {
-        mockMvc.perform(get("/shop/search/price")
+    public void testReturnPriceList4() throws Exception {
+        mockMvc.perform(get(URL)
                         .param("brandId", BRAND_ID)
                         .param("productId", PRODUCT_ID)
                         .param("dateTime", "2020-06-16T21:00:00"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.price").value(38.95));
+    }
+
+    @Test
+    public void testInvalidParamReturnMethodArgumentTypeMismatchException() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("productId", PRODUCT_ID)
+                        .param("dateTime", "2024-10-01T10:00:00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Param 'brandId' is mandatory"))
+                .andExpect(jsonPath("$.errorCode").value("400"));
+    }
+
+    @Test
+    public void testInvalidValueReturnMethodArgumentTypeMismatchException() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("brandId", "invalid")
+                        .param("productId", PRODUCT_ID)
+                        .param("dateTime", "2024-10-01T10:00:00")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Invalid value for param: 'brandId'"))
+                .andExpect(jsonPath("$.errorCode").value("400"));
+    }
+
+    @Test
+    public void testIncorrectParamReturnDateTimeParseException() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("brandId", BRAND_ID)
+                        .param("productId", PRODUCT_ID)
+                        .param("dateTime", "2024")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Cant parse dateTime as date"))
+                .andExpect(jsonPath("$.errorCode").value("400"));
     }
 }
